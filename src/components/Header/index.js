5d7da3aa74cce437c65b9996f6c0fcb0716a7ffc
Igor Veyner <igorveyner95@gmail.com>
2021-05-24 13:04:32 -0400

    create, stub and render web navagation component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import './index.css'
 import Logo from '../Logo'
+import Nav from '../Nav'
 
 const Header = () => {
   return (
     <header className="navbar">
       <Logo />
+      <Nav />
     </header>
   )
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <p>-Link-</p>
+      <p>-Link-</p>
+      <p>-Link-</p>
+    </nav>
+  )
+}
+
+export default Navbar
