1c8aa7ae444e11721467260535a1ea1c33347514
Igor Veyner <igorveyner95@gmail.com>
2021-05-26 16:44:27 -0400

    add and stub IconNav component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import './index.css'
 import Logo from '../Logo'
 import Nav from '../Nav'
+import IconNav from '../IconNav'
 
 const Header = () => {
   return (
     <header className="navbar">
       <Logo />
       <Nav />
+      <IconNav />
     </header>
   )
 }
---
+++ src/components/IconNav/index.js
@@ -0,0 +1,11 @@
+const IconNav = () => {
+  return (
+    <nav>
+      <p>-Icon-</p>
+      <p>-Icon-</p>
+      <p>-Icon-</p>
+    </nav>
+  )
+}
+
+export default IconNav
