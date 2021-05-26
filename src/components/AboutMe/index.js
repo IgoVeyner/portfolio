45ef2762b390f3615e48e08e59975a2ec3935848
Igor Veyner <igorveyner95@gmail.com>
2021-05-26 16:55:23 -0400

    add and stub out AboutMe section

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Lorem Ipsum</p>
+    </div>
+  )
+}
+
+export default AboutMe
--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,12 +1,14 @@
 import "./index.css"
 import IntroText from '../IntroText'
 import ProjectSummary from "../ProjectSummary"
+import AboutMe from "../AboutMe"
 
 const MainContent = () => {
   return (
     <div className="main-content">
       <IntroText />
       <ProjectSummary />
+      <AboutMe />
     </div>
   )
 }
