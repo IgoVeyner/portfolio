6bd6ec4e1ac278345f95eb6d7b676e8638cbff81
Igor Veyner <igorveyner95@gmail.com>
2021-05-29 17:55:52 -0400

    add theme colors to mobile nav and add 1 second animation to all color shift

--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -1,9 +1,13 @@
 import './index.css'
 import IconNav from '../IconNav'
 import ThemeButton from '../ThemeButton'
+import { useSelector } from 'react-redux'
 
 const MobileNav = ({ menuExpanded }) => {
   let status = menuExpanded === "unmounting" ? "closed" : ""
+  const theme = useSelector(state => state.theme)
+
+  const navTheme = theme === "dark" ? "nav-dark" : "nav-light"
 
   return (
     <nav className={`mobile-nav open ${status}`}>
