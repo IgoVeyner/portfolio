df00bf9af048cd7879f797c9c94851b309e18654
Igor Veyner <igorveyner95@gmail.com>
2021-05-30 16:06:45 -0400

    update intro title animations

--- src/components/IntroText/index.js
+++ src/components/IntroText/index.js
@@ -8,14 +8,18 @@ const IntroText = () => {
           <h3 className="name">Igor Veyner</h3>
         </div>
 
-        <div className="rotate-container">
-          <ul className="rotating">
-            <li className="li-rotate">Frontend</li>
-            <li className="li-rotate">Backend</li>
-            <li className="li-rotate">Full Stack</li>
-          </ul>
+        <div className="title-container">
+          <div className="rotate-container">
+            <ul className="rotating">
+              <li className="li-rotate">Frontend</li>
+              <li className="li-rotate">Backend</li>
+              <li className="li-rotate">Full Stack</li>
+            </ul>
+          </div>
+
+          <span className="line"></span>
         </div>
-        
+
         <span className="rotate-span">Engineer</span>
       </div>
     </section>
