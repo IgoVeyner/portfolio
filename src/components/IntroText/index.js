e30d5ce21df9281ec5ff5232349e172b57a93f75
Igor Veyner <igorveyner95@gmail.com>
2021-05-30 15:19:43 -0400

    update animations

--- src/components/IntroText/index.js
+++ src/components/IntroText/index.js
@@ -2,21 +2,22 @@ import "./index.css"
 
 const IntroText = () => {
   return (
-    <section className="section intro-title">
-      <div className="name-container">
-        <h3 className="name">Igor Veyner</h3>
-      </div>
+    <section className="section">
+      <div className="intro-title">
+        <div className="name-container">
+          <h3 className="name">Igor Veyner</h3>
+        </div>
 
-      <div className="rotate-container">
-        <ul className="rotating">
-          <li className="li-rotate">Frontend</li>
-          <li className="li-rotate">Backend</li>
-          <li className="li-rotate">Full Stack</li>
-        </ul>
+        <div className="rotate-container">
+          <ul className="rotating">
+            <li className="li-rotate">Frontend</li>
+            <li className="li-rotate">Backend</li>
+            <li className="li-rotate">Full Stack</li>
+          </ul>
+        </div>
         
-        <span> Engineer</span>
+        <span className="rotate-span">Engineer</span>
       </div>
-
     </section>
   )
 }
