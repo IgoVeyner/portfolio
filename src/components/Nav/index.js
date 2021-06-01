5b3cf8efa9844f25726e5ab98b0cbb5641c020cb
Igor Veyner <igorveyner95@gmail.com>
2021-06-01 17:34:45 -0400

    add theme color

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,17 @@
+import { useSelector } from 'react-redux'
 import "./index.css"
 import IconNav from '../IconNav'
 
 const Navbar = () => {
+  const theme = useSelector(state => state.theme)
+  const linkColor = theme === "dark" ? "dark-link" : "light-link"
+
   return (
     <nav className="web-nav">
       <div className="web-nav-list">
-        <a href="#projects" className="web-link">Projects</a>
-        <a href="#about" className="web-link">About</a>
-        <a href="#contact" className="web-link">Contact</a>
+        <a href="#projects" className={`web-link ${linkColor}`}>Projects</a>
+        <a href="#about" className={`web-link ${linkColor}`}>About</a>
+        <a href="#contact" className={`web-link ${linkColor}`}>Contact</a>
       </div>
       <IconNav classNames={["web-icons"]} />
     </nav>
