9c0ddef65c50dbe209afb1a9cf7ae2e3525cc87a
Igor Veyner <igorveyner95@gmail.com>
2021-06-03 15:12:15 -0400

    use React-spring to animate button transition

---
+++ src/components/AnimatedIcon/index.js
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const AnimatedIcon = ({ theme }) => {
+  const properties = {
+    sun: {
+      r: 9,
+      transform: "rotate(40deg)",
+      cx: 12,
+      cy: 4,
+      opacity: 0
+    },
+    moon: {
+      r: 5,
+      transform: "rotate(90deg)",
+      cx: 30,
+      cy: 0,
+      opacity: 1
+    }
+  }
+
+  const { r, transform, cx, cy, opacity } = properties[
+    theme === "dark" ? "sun" : "moon"
+  ]
+
+  const svgContainerProps = useSpring({ transform })
+  const centerCircleProps = useSpring({ r })
+  const maskedCircleProps = useSpring({ cx, cy })
+  const linesProps = useSpring({ opacity })
+
+  return (
+    <animated.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke="currentColor"
+      style={{
+        cursor: "pointer",
+        ...svgContainerProps
+      }}
+    >
+      <mask id="myMask2">
+        <rect x="0" y="0" width="100%" height="100%" fill="white" />
+        <animated.circle style={maskedCircleProps} r="9" fill="black" />
+      </mask>
+
+      <animated.circle
+        cx="12"
+        cy="12"
+        style={centerCircleProps}
+        fill="black"
+        mask="url(#myMask2)"
+      />
+      <animated.g stroke="currentColor" style={linesProps}>
+        <line x1="12" y1="1" x2="12" y2="3" />
+        <line x1="12" y1="21" x2="12" y2="23" />
+        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+        <line x1="1" y1="12" x2="3" y2="12" />
+        <line x1="21" y1="12" x2="23" y2="12" />
+        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+      </animated.g>
+    </animated.svg>
+  );
+};
+
+export default AnimatedIcon
--- src/components/ThemeButton/index.js
+++ src/components/ThemeButton/index.js
@@ -1,6 +1,7 @@
 import './index.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { setLight, setDark } from '../../redux/actions/themeActions'
+import AnimatedIcon from '../AnimatedIcon'
 
 const ThemeButton = ({ classNames }) => {
   const [buttonName] = classNames
@@ -15,30 +16,7 @@ const ThemeButton = ({ classNames }) => {
       className={buttonName}
       onClick={toggleTheme}
       >
-      <svg 
-        className="theme-svg"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <mask id="mask">     
-          <rect x="0" y="0" width="100%" height="100%" fill="white" />  
-          <circle cx="12" cy="4" r="9" fill="black" />    
-        </mask>
-        <circle fill="black" cx="12" cy="12" r="9" mask="url(#mask)"/>
-	      <g stroke="currentColor">
-          {/* <line x1="12" y1="1" x2="12" y2="3" />
-          <line x1="12" y1="21" x2="12" y2="23" />
-          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-          <line x1="1" y1="12" x2="3" y2="12" />
-          <line x1="21" y1="12" x2="23" y2="12" />
-          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /> */}
-        </g>
-      </svg>
+      <AnimatedIcon theme={theme} />
     </button>
   )
 }
