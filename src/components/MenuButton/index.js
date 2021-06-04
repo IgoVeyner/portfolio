400bb5ebb5e9f07c6d6a94af3a64dab0321adf54
Igor Veyner <igorveyner95@gmail.com>
2021-06-04 14:46:57 -0400

    add hover effect for mobile menu

--- src/components/MenuButton/index.js
+++ src/components/MenuButton/index.js
@@ -4,13 +4,14 @@ import './index.css'
 const MenuButton = ({ toggleMenu, menuExpanded }) => {
   const theme = useSelector(state => state.theme)
   const menuColor = theme === "dark" ? "dark-menu" : "light-menu"
+  const menuBGColor = theme === "dark" ? "dark-bg-menu" : "light-bg-menu"
 
   const cases = ["unmounted", "unmounting"]
   const menuStatus = cases.includes(menuExpanded) ? "" : "menu-open"
   
   return (
     <div 
-      className={`menu ${menuStatus}`}
+      className={`menu ${menuStatus} ${menuBGColor}`}
       onClick={toggleMenu}
       >
       <div class="hamburger" >
