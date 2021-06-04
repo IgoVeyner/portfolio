486fda39fa80addc4d1726ecb052d9918baa4b75
Igor Veyner <igorveyner95@gmail.com>
2021-06-04 13:29:16 -0400

    add color change based on theme

--- src/components/MenuButton/index.js
+++ src/components/MenuButton/index.js
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux'
 import './index.css'
 
 const MenuButton = ({ toggleMenu, menuExpanded }) => {
+  const theme = useSelector(state => state.theme)
+  const menuColor = theme === "dark" ? "dark-menu" : "light-menu"
+
   const cases = ["unmounted", "unmounting"]
   const menuStatus = cases.includes(menuExpanded) ? "" : "menu-open"
   
@@ -10,9 +14,9 @@ const MenuButton = ({ toggleMenu, menuExpanded }) => {
       onClick={toggleMenu}
       >
       <div class="hamburger" >
-        <span class="menu-line"></span>
-        <span class="menu-line"></span>
-        <span class="menu-line"></span>
+        <span class={`menu-line ${menuColor}`}></span>
+        <span class={`menu-line ${menuColor}`}></span>
+        <span class={`menu-line ${menuColor}`}></span>
       </div>
     </div>
   )
