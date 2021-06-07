e507a73d12d2e29192e3a80ee2907fe606f829b2
Igor Veyner <igorveyner95@gmail.com>
2021-06-07 17:53:48 -0400

    add typing effect

--- src/components/TitleTyping/index.js
+++ src/components/TitleTyping/index.js
@@ -1,13 +1,44 @@
+import React, { useState, useEffect } from "react";
 import "./index.css"
 
+const words = ["Engineer", "Developer"]
+
 const TitleTyping = ({ titleColor }) => {
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [reverse, setReverse] = useState(false);
+
+  useEffect(() => {
+    if (index === words.length) return;
+
+    if ( subIndex === words[index].length + 1 && 
+        index !== words.length - 1 && !reverse ) {
+      setReverse(true);
+      return;
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => prev + 1);
+      return;
+    }
+
+
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 :
+                150, parseInt(Math.random() * 1000)));
+
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, reverse]);
+
   return (
     <div>
       <span className="linux">
           ~$
       </span> 
       <span className={`title-ending ${titleColor} terminal`}>
-        Engineer
+        {`${words[index].substring(0, subIndex)}`}
       </span>
     </div>
   )
