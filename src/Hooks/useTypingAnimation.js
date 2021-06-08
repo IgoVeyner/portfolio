742c22e23a0c9794efcbc51996c86ea61d58f216
Igor Veyner <igorveyner95@gmail.com>
2021-06-08 07:14:43 -0400

    refactor to custom hook file

---
+++ src/Hooks/useTypingAnimation.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+const useTypingAnimation = (words, index, setIndex, reverse, setReverse, subIndex, setSubIndex) => {
+  useEffect(() => {
+    if (subIndex === words[index].length + 1 && !reverse ) {
+      setReverse(true);
+      return;
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      index === words.length - 1 ? setIndex(0) : setIndex((prev) => prev + 1)
+      return;
+    }
+
+
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 :
+                150, parseInt(Math.random() * 1000)));
+
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, reverse]);
+}
+
+export default useTypingAnimation
--- src/components/TitleTyping/index.js
+++ src/components/TitleTyping/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import useTypingAnimation from '../../Hooks/useTypingAnimation'
 import "./index.css"
 
 const words = ["Engineer", "Developer"]
@@ -8,26 +9,7 @@ const TitleTyping = ({ titleColor }) => {
   const [subIndex, setSubIndex] = useState(0);
   const [reverse, setReverse] = useState(false);
 
-  useEffect(() => {
-    if (subIndex === words[index].length + 1 && !reverse ) {
-      setReverse(true);
-      return;
-    }
-
-    if (subIndex === 0 && reverse) {
-      setReverse(false);
-      index === words.length - 1 ? setIndex(0) : setIndex((prev) => prev + 1)
-      return;
-    }
-
-
-    const timeout = setTimeout(() => {
-      setSubIndex((prev) => prev + (reverse ? -1 : 1));
-    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 :
-                150, parseInt(Math.random() * 1000)));
-
-    return () => clearTimeout(timeout);
-  }, [subIndex, index, reverse]);
+  useTypingAnimation(words, index, setIndex, reverse, setReverse, subIndex, setSubIndex)
 
   return (
     <div>
