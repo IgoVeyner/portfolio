a837284ea75e3fbc031229574adab9a74071ff9f
Igor Veyner <igorveyner95@gmail.com>
2021-06-09 07:31:36 -0400

    render project info

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,7 +1,15 @@
-const Project = () => {
+const Project = ({ project }) => {
+  const projectName = project.projectName
+  const description = project.description
+  const github = project.github
+  const demo = project.demoLink
+  
   return (
     <div className="project">
-      
+      <h1>{projectName}</h1>
+      <p>{description}</p>
+      <a href={github}>GitHub</a>
+      <a href={demo}>Demo</a>
     </div>
   )
 }
