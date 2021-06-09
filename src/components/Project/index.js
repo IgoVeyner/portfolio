5faa8872d496a89fb30d7fa35a87f61a083a4dd0
Igor Veyner <igorveyner95@gmail.com>
2021-06-09 11:14:11 -0400

    add placeholder images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,14 +5,21 @@ const Project = ({ project, id }) => {
   
   return (
     <section className="section" id={id}>
-      <div className="project-preview">
-
-      </div>
-      <div className="project-description">
-        <h1>{projectName}</h1>
-        <p>{description}</p>
-        <a href={github}>GitHub</a>
-        <a href={demoLink}>Demo</a>
+      <div className="project">
+        <div className="project-preview">
+          <img 
+            src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" 
+            alt="stub"
+            width="400px"
+            height="400px"
+          />
+        </div>
+        <div className="project-description">
+          <h1>{projectName}</h1>
+          <p>{description}</p>
+          <a href={github}>GitHub</a>
+          <a href={demoLink}>Demo</a>
+        </div>
       </div>
     </section>
   )
