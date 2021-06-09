61747bacdd80fbec442d2286da858ea15b49da0e
Igor Veyner <igorveyner95@gmail.com>
2021-06-09 07:35:18 -0400

    refactor

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,15 +1,12 @@
 const Project = ({ project }) => {
-  const projectName = project.projectName
-  const description = project.description
-  const github = project.github
-  const demo = project.demoLink
+  const {projectName, description, github, demoLink } = project
   
   return (
     <div className="project">
       <h1>{projectName}</h1>
       <p>{description}</p>
       <a href={github}>GitHub</a>
-      <a href={demo}>Demo</a>
+      <a href={demoLink}>Demo</a>
     </div>
   )
 }
