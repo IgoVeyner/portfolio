bebffa4d7d334fbd4e347cd4faddbb94be26c2c2
Igor Veyner <igorveyner95@gmail.com>
2021-06-10 13:01:51 -0400

    refactor icon

--- src/components/IconNav/index.js
+++ src/components/IconNav/index.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
 import GithubIcon from '../GithubIcon'
+import LinkedInIcon from '../LinkedInIcon'
 import MediumIcon from '../MediumIcon'
 import "./index.css"
 
@@ -10,18 +11,9 @@ const IconNav = ({ classNames }) => {
 
   return (
     <nav className={navName} >
-      <MediumIcon link={"https://igor-veyner.medium.com/"} iconColor={iconColor} />
-      <GithubIcon link={"https://github.com/IgoVeyner"} iconColor={iconColor} />
-
-      <a href="https://www.linkedin.com/in/igorveyner/" className="icon-container" id="linkedIn" target="_blank" rel="noreferrer">
-        <svg className={`icon ${iconColor} medium`}>
-          <path 
-            d="M0 0v24h24v-24h-24zm8 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 
-            1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 
-            0v5.604h-3v-11h3v1.765c1.397-2.586 7-2.777 7 2.476v6.759z"
-          />
-        </svg>
-      </a>
+      <MediumIcon link="https://igor-veyner.medium.com/" iconColor={iconColor} />
+      <GithubIcon link="https://github.com/IgoVeyner" iconColor={iconColor} />
+      <LinkedInIcon link="https://www.linkedin.com/in/igorveyner/" iconColor={iconColor} />
     </nav>
   )
 }
