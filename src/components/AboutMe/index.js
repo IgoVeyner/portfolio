13e7e1921617736924428d45af86b8b62035fdd0
Igor Veyner <igorveyner95@gmail.com>
2021-06-13 12:07:48 -0400

    add layout and element styles

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,24 +1,27 @@
+import './index.css'
 import selfie from '../../images/selfie.JPG'
 
 const AboutMe = () => {
   return (
     <section className="section" id="about">
-      <div className="about-image">
-        <img src={selfie} alt="selfie" className="selfie-img" />
-      </div>
-      <div className="about-text">
-        <h2>Hello!</h2>
-        <p>
-          My name is Igor and I'm a NYC based Full-Stack Software Engineer!
-          I'm a proud graduate of The Flatiron School where I learned Ruby on Rails for 
-          Backend and React for Frontend development. If you're interested in learning 
-          more about my skills and technologies I use check out my skills section.
-        </p>
-        <p>
-          I enjoy learning new technologies, starting new projects, and teaching.
-          When I'm not sitting in front of a computer screen you can find me 
-          skateboarding in the streets and parks across New York.
-        </p>
+      <div className="about">
+        <div className="about-image">
+          <img src={selfie} alt="selfie" className="selfie-img" />
+        </div>
+        <div className="about-text">
+          <h2>Hello!</h2>
+          <p>
+            My name is Igor and I'm a NYC based Full-Stack Software Engineer!
+            I'm a proud graduate of The Flatiron School where I learned Ruby on Rails for 
+            Backend and React for Frontend development. If you're interested in learning 
+            more about my skills and technologies I use check out my skills section.
+          </p>
+          <p>
+            I enjoy learning new technologies, starting new projects, and teaching.
+            When I'm not sitting in front of a computer screen you can find me 
+            skateboarding in the streets and parks across New York.
+          </p>
+        </div>
       </div>
     </section>
   )
