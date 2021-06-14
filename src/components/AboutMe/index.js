664986dae839281dc1abbb39e6aba71ccf607747
Igor Veyner <igorveyner95@gmail.com>
2021-06-14 17:47:23 -0400

    update styles for text and layout

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -8,16 +8,18 @@ const AboutMe = () => {
         <div className="about-image">
           <img src={selfie} alt="selfie" className="selfie-img" />
         </div>
-        <div className="about-text">
-          <h2>Hello!</h2>
-          <p>
-            My name is Igor and I'm a NYC based Full-Stack Software Engineer!
-            I'm a proud graduate of The Flatiron School where I learned Ruby on Rails for 
-            Backend and React for Frontend development. If you're interested in learning 
-            more about my skills and technologies I use check out my skills section.
+        <div className="about-text-container">
+          <h2 className="about-title">Hello!</h2>
+          <p className="about-text">
+            My name is Igor.
           </p>
-          <p>
-            I enjoy learning new technologies, starting new projects, and teaching.
+          <p className="about-text">
+            I'm a NYC based Full-Stack Software Engineer and 
+            a proud graduate of The Flatiron School where I learned Ruby on Rails for 
+            Backend and React for Frontend development. If you're interested in learning 
+            more about my skills, click here.
+          </p>
+          <p className="about-text">
             When I'm not sitting in front of a computer screen you can find me 
             skateboarding in the streets and parks across New York.
           </p>
