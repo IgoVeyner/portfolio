ebf70c0f32f4f8eac9b4616ee7d3df393ad6c153
Igor Veyner <igorveyner95@gmail.com>
2021-06-14 18:56:25 -0400

    add theme based color and link color

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux'
 import './index.css'
 import selfie from '../../images/selfie.JPG'
 
 const AboutMe = () => {
+  const theme = useSelector(state => state.theme)
+  let [aboutTitle, aboutText] = ["dark-about-title", "dark-about-text"]
+  
+  if (theme === "light") {
+    [aboutTitle, aboutText] = ["light-about-title", "light-about-text"]
+  }
+
+
   return (
     <section className="section section-about" id="about">
       <div className="about">
@@ -9,21 +18,21 @@ const AboutMe = () => {
           <img src={selfie} alt="selfie" className="selfie-img" />
         </div>
         <div className="about-text-container">
-          <h2 className="about-title">Hello!</h2>
-          <p className="about-text">
+          <h2 className={`about-title ${aboutTitle}`}>Hello!</h2>
+          <p className={`about-text ${aboutText}`}>
             My name is Igor.
           </p>
-          <p className="about-text">
+          <p className={`about-text ${aboutText}`}>
             I'm a NYC based Full-Stack Software Engineer and 
-            a proud graduate of 
-            <a href="https://flatironschool.com/" target="_blank" rel="noreferrer">
-              The Flatiron School
-            </a> 
-            where I learned Ruby on Rails for 
+            a proud graduate of <a href="https://flatironschool.com/"
+              className="about-link" target="_blank" rel="noreferrer">
+              The Flatiron School 
+            </a> where I learned Ruby on Rails for 
             Backend and React for Frontend development. If you're interested in learning 
-            more about my skills, click <a href="#skills">here</a>.
-          </p>
-          <p className="about-text">
+            more about my skills, click <a href="#skills" className="about-link">
+              here
+          </a>.</p>
+          <p className={`about-text ${aboutText}`}>
             When I'm not sitting in front of a computer screen you can find me 
             skateboarding in the streets and parks across New York.
           </p>
