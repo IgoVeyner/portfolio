9bf8469175bdf7886ad91c799f8b4610248a1788
Igor Veyner <igorveyner95@gmail.com>
2021-06-15 18:05:42 -0400

    add lists to skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,8 +2,24 @@ const Skills = () => {
   return (
     <section className="section">
         <h1>Technical Skills</h1>
+
         <h2>Frontend</h2>
+        <ul>
+          <li>React & Redux</li>
+          <li>JavaScript (ES6)</li>
+          <li>jQuery</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+
         <h2>Backend</h2>
+        <ul>
+          <li>Ruby</li>
+          <li>Ruby on Rails</li>
+          <li>SQLite</li>
+          <li>PostgreSQL</li>
+        </ul>
+
     </section>
   )
 }
