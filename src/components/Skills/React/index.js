050d7bea2385173b3b682573590009634ec00200
Igor Veyner <igorveyner95@gmail.com>
2021-06-16 16:09:41 -0400

    add filter hover effect

--- src/components/Skills/React/index.js
+++ src/components/Skills/React/index.js
@@ -4,7 +4,7 @@ import ReactImage from '../../../images/React.png'
 const ReactLi = () => {
   return (
     <li>
-      <img src={ReactImage} className="react-image" alt="React"/>
+      <img src={ReactImage} className="skills-image react-image" alt="React"/>
       <p>React</p>
       <div className="react-progress"></div>
     </li>
