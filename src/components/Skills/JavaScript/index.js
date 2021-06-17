2c5c4aba49018284f7226d25ef942f8633f358fa
Igor Veyner <igorveyner95@gmail.com>
2021-06-17 11:50:48 -0400

    add javascriptLi component

---
+++ src/components/Skills/JavaScript/index.js
@@ -0,0 +1,11 @@
+const JavaScriptLi = () => {
+  return (
+    <li>
+      <img src={ReduxImage} className="skills-image javascript-image" alt="javascript"/>
+      <p>JavaScript (ES6)</p>
+      <div className="skills-progress javascript-progress"></div>
+    </li>
+  )
+}
+
+export default JavaScriptLi
