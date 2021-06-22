0bbf363dcc180a3387f1bc0c0fa4139ed6f90f41
Igor Veyner <igorveyner95@gmail.com>
2021-06-22 09:20:55 -0400

    abstract project object creation to new file

--- src/components/Projects/ProjectSummary/index.js
+++ src/components/Projects/ProjectSummary/index.js
@@ -1,44 +1,16 @@
 import { useSelector } from 'react-redux'
 import Project from "../Project"
-import thrasher from '../../../images/thrasher-forums.png'
-import typing from '../../../images/typing.gif'
-import central from '../../../images/central.png'
-
-const projects = [
-  {
-    projectName: "Thrasher Forums",
-    description: "Skateboarding Forum / Message board site in the style of Thrasher Magazine",
-    demoLink: "https://www.youtube.com/watch?v=21PHZBzQKAo",
-    github: "https://github.com/IgoVeyner/thrasher-forums",
-    img: thrasher
-  },
-  {
-    projectName: "Code Speed Typer",
-    description: "Typing Tutor application that aims to improve a programmer's typing speed",
-    demoLink: "https://youtu.be/48r0azXDRM4",
-    github: "https://github.com/IgoVeyner/code-speed-typer",
-    img: typing
-  },
-  {
-    projectName: "Central Park Zoo",
-    description: "A redesign of NYC's Central Park Zoo website to allow for a virtual zoo experience in the COVID era",
-    demoLink: "https://youtu.be/JnPUEg8UXuY",
-    github: "https://github.com/IgoVeyner/central_park_zoo",
-    img: central
-  }
-]
-
+import projects from '../projects'
 
 const renderProjects = (theme) =>{
   return projects.map((project, i) => { 
     return (
       <Project project={project} id={`project-${i+1}`} key={`project-${i+1}`}
         reverse={i % 2 === 0 ? false : true} theme={theme}
-        /> 
+      /> 
     )
   })
 }
-
 
 const ProjectSummary = () => {
   const theme = useSelector(state => state.theme)
