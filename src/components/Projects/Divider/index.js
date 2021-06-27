ad3a40ceee85ffbfe4bfe21b1ed6541a5655a3c7
Igor Veyner <igorveyner95@gmail.com>
2021-06-27 12:50:35 -0400

    add svg animation

--- src/components/Projects/Divider/index.js
+++ src/components/Projects/Divider/index.js
@@ -1,8 +1,17 @@
+import './index.css'
+
 const Divider = () => {
   return (
-    <svg width="110" height="7" viewBox="0 0 110 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5.14286 3H72L66.8571 7H0L5.14286 3Z" fill="#4AF626"/>
-      <path d="M43.1429 0H110L104.857 4H38L43.1429 0Z" fill="#4AF626"/>
+    <svg 
+      width="110" height="7" viewBox="0 0 110 7" className="divider-container">
+      <path 
+        className="divider divider-top"
+        d="M43.1429 0H110L104.857 4H38L43.1429 0Z" 
+      />
+      <path 
+        className="divider divider-bottom"
+        d="M5.14286 3H72L66.8571 7H0L5.14286 3Z" 
+      />
     </svg>
   )
 }
