ed8c9289cc7c5841f6145818af5e285af09924a2
Igor Veyner <igorveyner95@gmail.com>
2021-07-14 09:12:25 -0400

    add dispatch to update modal status in redux store

--- src/components/Projects/Modal/index.js
+++ src/components/Projects/Modal/index.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { setOpen, setClose } from '../../../redux/actions/modalActions'
 import ModalImage from "../ModalImage";
 
 const Modal = ({ image, orderReverse }) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const modalStatus = useSelector(state => state.modal)
+  const dispatch = useDispatch()
   
-  const toggleModal = () => setIsOpen(!isOpen)
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+    dispatch(modalStatus ? setClose() : setOpen())
+  }
 
   return (
     <div className={`project-preview ${orderReverse}`}>
