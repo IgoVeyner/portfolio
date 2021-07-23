00d1647c47a27507cea54a292766820a41c986f9
Igor Veyner <igorveyner95@gmail.com>
2021-07-23 08:38:53 -0400

    move logic to reducer from action

--- src/redux/reducers/carouselReducer.js
+++ src/redux/reducers/carouselReducer.js
@@ -1,10 +1,12 @@
 const handleCarousel = (state = 0, action) => {
+  const length = 2
+
   switch (action.type) {
     case "SET_INCREASE":
-      return action.payload
+      return action.payload > length ? 0 : action.payload
 
     case "SET_DECREASE":
-      return action.payload
+      return action.payload < 0 ? length : action.payload
 
     case "SET_FROM_INDICATOR":
       return action.payload
